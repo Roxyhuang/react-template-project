6666ccf1a527f55f2fc52fd552ea8990dd201475
neo_huang <sixin_huang@outlook.com>
2017-09-01 21:14:57 +0800

    modify app.jsx

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import Container from './components/Container';
 import errorHandler from '../core/utils/errorHandler';
-import g from './App.scss';
+import sty from './App.scss';
 
 const App = (props) => {
   errorHandler.initRouter(props.router);
-  return <Container className={g.placeholder} router={props.router}>{props.children}</Container>;
+  return <Container className={sty.placeholder} router={props.router}>{props.children}</Container>;
 };
 
 export default App;
